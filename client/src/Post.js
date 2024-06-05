ad0b76bd7dcc69f8d5c0e2076f7c0a74f8fcfb0d
LAPTOP-PK8JHPSO\ADMIN <linhnhi.2922@gmail.com>
2024-06-05 20:34:07 +0700

    login-logout

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post () {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://scontent.fdad1-1.fna.fbcdn.net/v/t39.30808-6/447852585_452245730835494_5654339456513346602_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=i0yoVuAEQT4Q7kNvgGU4BBU&_nc_ht=scontent.fdad1-1.fna&oh=00_AYA3q67UU55X28kES8i7ZsKj5QCBZefkMRpmzlhAtJjKwA&oe=6665DB54"/>
+            </div>
+            <div className="texts">
+                <h2>THẦN CHÚ FIX BUG CHO MẤY BẠN CODE PYTHON</h2>
+                <p className="info">
+                    <a className="author">Minh Tri</a>
+                    <time>2024-04-05 12:15</time>
+                </p>
+                <p className="summary">Anh em lập trình sử dụng SQL Server rất nhiều người đang không biết điều này - kinh nghiệm phỏng vấn và làm dự án của tôi</p>
+            </div>
+      </div> 
+    );
+}
